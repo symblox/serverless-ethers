54910250bd7207500742ba491417341d88339518
Symblox Dev <symblox@protonmail.com>
2020-10-12 22:07:41 +0800

    Get price script

--- functions/exec.js
+++ functions/exec.js
@@ -2,6 +2,9 @@ const axios = require("axios");
 const ethers = require("ethers");
 const { abis, addresses } = require("../contracts");
 
+const sellSyxAmount = ethers.utils.parseEther("1");
+const sellVlxAmount = ethers.utils.parseEther("1");
+
 exports.handler = async function () {
   console.log("Starting...");
   // Load Contract ABIs
@@ -29,6 +32,111 @@ exports.handler = async function () {
     console.log(`wvlx total balance: ${wvlxBalance / 10 ** 18} vlx`);
     const syxBalance = await contract.getBalance(addresses.syx);
     console.log(`syx total balance: ${syxBalance / 10 ** 18} syx`);
+
+    let tokenAmountIn,
+      tokenAmountOut,
+      spotPrice,
+      tokenBalanceIn,
+      tokenBalanceOut,
+      balanceIn,
+      denormIn,
+      balanceOut,
+      denormOut,
+      tokenIn,
+      tokenOut,
+      swapFee;
+    tokenIn = addresses.wvlx;
+    tokenOut = addresses.syx;
+    swapFee = await contract.getSwapFee();
+    balanceIn = await contract.getBalance(tokenIn);
+    denormIn = await contract.getDenormalizedWeight(tokenIn);
+    balanceOut = await contract.getBalance(tokenOut);
+    denormOut = await contract.getDenormalizedWeight(tokenOut);
+
+    tokenAmountOut = await contract.calcOutGivenIn(
+      balanceIn,
+      denormIn,
+      balanceOut,
+      denormOut,
+      sellSyxAmount.toString(),
+      swapFee
+    );
+
+    tokenBalanceIn =
+      parseFloat(ethers.utils.formatEther(balanceIn).toString()) +
+      parseFloat(ethers.utils.formatEther(sellSyxAmount).toString());
+    tokenBalanceOut =
+      parseFloat(ethers.utils.formatEther(balanceOut).toString()) -
+      parseFloat(ethers.utils.formatEther(tokenAmountOut).toString());
+    // console.log(tokenIn, tokenOut);
+    // console.log(
+    //   parseFloat(ethers.utils.formatEther(balanceIn).toString()),
+    //   parseFloat(ethers.utils.formatEther(sellVlxAmount).toString())
+    // );
+    // console.log(
+    //   parseFloat(ethers.utils.formatEther(balanceOut).toString()),
+    //   parseFloat(ethers.utils.formatEther(tokenAmountOut).toString())
+    // );
+    spotPrice = await contract.calcSpotPrice(
+      ethers.utils.parseEther(tokenBalanceIn.toString()),
+      denormIn,
+      ethers.utils.parseEther(tokenBalanceOut.toString()),
+      denormOut,
+      swapFee
+    );
+
+    console.log(
+      `sell ${ethers.utils.formatEther(
+        sellSyxAmount
+      )} SYX. price is 1 SYX = ${ethers.utils.formatEther(spotPrice)} VLX`
+    );
+
+    tokenIn = addresses.syx;
+    tokenOut = addresses.wvlx;
+    swapFee = await contract.getSwapFee();
+    balanceIn = await contract.getBalance(tokenIn);
+    denormIn = await contract.getDenormalizedWeight(tokenIn);
+    balanceOut = await contract.getBalance(tokenOut);
+    denormOut = await contract.getDenormalizedWeight(tokenOut);
+
+    tokenAmountOut = await contract.calcOutGivenIn(
+      balanceIn,
+      denormIn,
+      balanceOut,
+      denormOut,
+      sellVlxAmount.toString(),
+      swapFee
+    );
+
+    tokenBalanceIn =
+      parseFloat(ethers.utils.formatEther(balanceIn).toString()) +
+      parseFloat(ethers.utils.formatEther(sellVlxAmount).toString());
+    tokenBalanceOut =
+      parseFloat(ethers.utils.formatEther(balanceOut).toString()) -
+      parseFloat(ethers.utils.formatEther(tokenAmountOut).toString());
+
+    // console.log(tokenIn, tokenOut);
+    // console.log(
+    //   parseFloat(ethers.utils.formatEther(balanceIn).toString()),
+    //   parseFloat(ethers.utils.formatEther(sellVlxAmount).toString())
+    // );
+    // console.log(
+    //   parseFloat(ethers.utils.formatEther(balanceOut).toString()),
+    //   parseFloat(ethers.utils.formatEther(tokenAmountOut).toString())
+    // );
+    spotPrice = await contract.calcSpotPrice(
+      ethers.utils.parseEther(tokenBalanceIn.toString()),
+      denormIn,
+      ethers.utils.parseEther(tokenBalanceOut.toString()),
+      denormOut,
+      swapFee
+    );
+
+    console.log(
+      `sell ${ethers.utils.formatEther(
+        sellVlxAmount
+      )} VLX. price is 1 VLX = ${ethers.utils.formatEther(spotPrice)} SYX`
+    );
   } catch (err) {
     const errorMessage = `:warning: Transaction failed: ${err.message}`;
     console.error(errorMessage);
